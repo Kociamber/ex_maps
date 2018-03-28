d1e2f2026df13a6c67d8a8237fa56888ac78b04b
Rafal Kociszewski <rafal.kociszewski@gmail.com>
2018-03-28 11:26:54 +0200

    first commit

---
+++ lib/ex_maps.ex
@@ -0,0 +1,18 @@
+defmodule ExMaps do
+  @moduledoc """
+  Documentation for ExMaps.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExMaps.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_maps_test.exs
@@ -0,0 +1,8 @@
+defmodule ExMapsTest do
+  use ExUnit.Case
+  doctest ExMaps
+
+  test "greets the world" do
+    assert ExMaps.hello() == :world
+  end
+end
