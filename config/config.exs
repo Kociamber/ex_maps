a0c764ce1fdd079b9341ede7fe23519ae34a6fe5
Kociamber <rafal.kociszewski@gmail.com>
2018-08-26 13:19:34 +0200

    re-factor config and update deps

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,8 @@
 use Mix.Config
 
-config :ex_maps, api_key: System.get_env("EX_MAPS_API_KEY")
+config :ex_maps, api_key: System.get_env("MAPS_API_KEY")
 
 config :ex_maps, ExMaps.Cache,
   adapter: Nebulex.Adapters.Local,
   n_shards: 2,
   gc_interval: 3600
-
-import_config "#{Mix.env()}.secret.exs"
